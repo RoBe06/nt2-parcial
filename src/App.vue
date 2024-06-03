97aefb69f05f4c8635bcdd8c498debc420b9e0d8
Rosario Begani <rochibega@gmail.com>
2024-06-03 20:33:26 -0300

    Parcial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="my-4">Parcial</h1>
+    <FormularioComponente />
+    <p>Respuestas: {{ respuestas }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormularioComponente from './components/FormularioComponente.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioComponente
+  },
+  data() {
+    return {
+      respuestas: {
+        1: 'c',
+        2: 'c',
+        3: 'a'
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
